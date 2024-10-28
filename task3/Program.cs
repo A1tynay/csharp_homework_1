934d395d83223bc1f2d49b7d3662ce0359e14729
Altynay <altynaytileumuratova@gmail.com>
2024-10-28 16:08:47 +0500

    Add solution Task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -2,3 +2,15 @@
 // число и выдает, является ли число четным
 // 4 => yes
 // 5 => no
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 2 == 0)
+{
+    System.Console.WriteLine("Yes");
+}
+else
+{
+   System.Console.WriteLine("No");   
+}
