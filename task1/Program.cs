7d6602d36bbec762cfd8168f6cb324aaf815d8c0
Altynay <altynaytileumuratova@gmail.com>
2024-10-28 15:27:19 +0500

    Add solution Task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,3 +2,18 @@
 // два числа и выдает, какое число большее, а какое 
 // меньшее
 // a = 5, b = 7  => max = 7, min = 5
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > secondNumber)
+{
+    System.Console.WriteLine(firstNumber);
+}
+else
+{
+    System.Console.WriteLine(secondNumber);
+}
