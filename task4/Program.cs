aaeddc089cff5fb0eb025c378effbe74556923d2
Altynay <altynaytileumuratova@gmail.com>
2024-10-28 16:18:42 +0500

    Add solution Task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -2,3 +2,14 @@
 // число (N), а на входе показывает все четные числа 
 // от 1 до N
 // 8 => 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= number; i++)
+{
+    if(i % 2 == 0)
+    {
+        System.Console.Write(i + " ");
+    }
+}
