d0784b07f4c01af9c7df31a6ff7757ad47185f50
Altynay <altynaytileumuratova@gmail.com>
2024-10-28 16:37:01 +0500

    Add solution Task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,3 +1,20 @@
 ﻿// Напишите программу, которая принимает на вход 
 // три числа и выдает максимальное из этих чисел.
 // 2, 3, 5    =>   5
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+
+System.Console.Write("max = " + max);
